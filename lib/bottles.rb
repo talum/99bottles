bba2de9520f1a20a7e4480a3b4f11eedc74ca118
talum <tracy.a.lum@gmail.com>
2018-06-03 21:10:43 -0400

    forward and remove stuff

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,34 +10,40 @@ class Bottles
   def verse(number)
     <<-VERSE
 #{count(number).to_s.capitalize} #{container(number)} of beer on the wall, #{count(number)} #{container(number)} of beer.
-#{action(number)}
+#{action(number)}, #{subsequent(number)} #{container(subsequent(number))} of beer on the wall.
 VERSE
   end
 
   def count(number)
-    BottleNumber.new.count(number)
+    BottleNumber.new(number).count
   end
 
   def pronoun(number)
-    BottleNumber.new.pronoun(number)
+    BottleNumber.new(number).pronoun
   end
 
   def subsequent(number)
-    BottleNumber.new.subsequent(number)
+    BottleNumber.new(number).subsequent
   end
 
   def container(number)
-    BottleNumber.new.container(number)
+    BottleNumber.new(number).container
   end
 
   def action(number)
-    BottleNumber.new.action(number)
+    BottleNumber.new(number).action
   end
 
 end
 
 class BottleNumber
-  def count(number)
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def count
     if number == 0
       "no more"
     else
@@ -45,7 +51,7 @@ class BottleNumber
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       "it"
     else
@@ -53,15 +59,17 @@ class BottleNumber
     end
   end
 
-  def subsequent(number)
-    if number == 1
+  def subsequent
+    if number == 0
+      "99"
+    elsif number == 1
       "no more"
     else
       number - 1
     end
   end
 
-  def container(number)
+  def container
     if number == 1
       "bottle"
     else
@@ -69,11 +77,11 @@ class BottleNumber
     end
   end
 
-  def action(number)
+  def action
     if number == 0
-      "Go to the store and buy some more, 99 bottles of beer on the wall."
+      "Go to the store and buy some more"
     else
-      "Take #{pronoun(number)} down and pass it around, #{subsequent(number)} #{container(subsequent(number))} of beer on the wall."
+      "Take #{pronoun} down and pass it around"
     end
    end
 end
